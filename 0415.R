42f2f4f9c9b11a2896d21f7b76114c90c42f9461
unknown <a3062930629@gmail.com>
2019-04-15 12:53:36 +0800

    second

--- 0415.R
+++ 0415.R
@@ -40,4 +40,51 @@ library(magrittr)
 tw20.day.ret<-Return.calculate(tw20.xts, method = "log")
 head(tw20.day.ret)
 
-#5
+#6
+#1101
+con = gzcon(url('http://www.systematicportfolio.com/sit.gz', 'rb'))
+source(con)
+close(con)
+
+bt.prep(data, align='keep.all')
+names(data)
+head(data$prices)
+tail(data$prices)
+data$weight[] <- 1
+buy.hold <- bt.run.share(data, clean.signal=F, trade.summary = TRUE)
+buy.hold <- bt.run(data)
+prices<-data$prices
+sma <- bt.apply(data, function(x) { SMA(Cl(x), 200) } ) 
+data$weight[] <- NA # update weights matirx
+data$weight[] <- iif(prices >= sma, 1, 0)
+sma.cross <- bt.run(data, trade.summary=T)   
+
+plotbt.custom.report(sma.cross, buy.hold)
+models<-list("SMA"= sma.cross, "BH" = buy.hold)
+strategy.performance.snapshoot(sma.cross, T) 
+strategy.performance.snapshoot(buy.hold, T) 
+strategy.performance.snapshoot(models, T)
+strategy.performance.snapshoot(models, control=list(comparison=T), sort.performance=T)
+#
+plotbt.strategy.sidebyside(models, return.table=T)
+
+
+data1<-new.env()
+data1$prices<-tw20.xts$`1101`
+prices<-data1$prices
+prices
+
+library(TTR)
+sma.cross <- bt.run(data, trade.summary=T)
+sma90<-SMA(prices, 90)
+head(sma90, 91)
+
+bt.prep(data1, align='keep.all')
+names(data1)
+data1$dates
+data1$prices
+data1$prices<-prices
+
+class(data1$dates)
+data1$weight
+data1$execution.price = prices
